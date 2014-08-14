971c0f44c2b99fe4e70c59aea8b4163d537138f2
patrickmcgovern <Patrick@lauries-mbp.attlocal.net>
2014-08-13 22:54:16 -0500

    one file example of code doc & cleanup for review

---
+++ README.md
@@ -0,0 +1,5 @@
+#PHP-AJP
+
+This package creates the ability to add monitoring for individual application servers load balanced using AJP (Apache), using using mod_proxy_balancer/mod_proxy_ajp.
+
+Copyright 2014 Justin Martin, TheFrozenFire
--- src/Ajp/Packet.php
+++ src/Ajp/Packet.php
@@ -1,57 +1,133 @@
 <?php
+/**
+ * Packet class
+ *
+ */
 namespace Ajp;
 
 use \RuntimeException;
+use \Ajp\PacketSerializer;
+use \Ajp\PacketParser;
 
 abstract class Packet implements PacketInterface
 {
-    protected static $serializerClass = '\Ajp\PacketSerializer';
-    
-    protected static $parserClass = '\Ajp\PacketParser';
-
+    /**
+     * @var string $headerCode Header Code
+     */
     protected $headerCode;
 
+    /**
+     * @var string $type Type
+     */
     protected $type;
-    
+
+    /**
+     * @var PacketSerializerInterface $serializer Serializer
+     */
     protected $serializer;
-    
+
+    /**
+     * @var PacketParserInterface $parser Parser
+     */
     protected $parser;
-    
+
+    /**
+     * Construct
+     *
+     * @todo move setter calls to DI
+     *
+     * @param PacketSerializerInterface $serializer
+     * @param PacketParserInterface $parser
+     */
     public function __construct(PacketSerializerInterface $serializer = null, PacketParserInterface $parser = null)
     {
-        $this->setSerializer(isset($serializer)?$serializer:new static::$serializerClass);
-        $this->setParser(isset($parser)?$parser:new static::$parserClass);
+        $this->setSerializer($serializer);
+        $this->setParser($parser);
     }
-    
+
+    /**
+     * Get header code
+     *
+     * @return string
+     */
     public function getHeaderCode()
     {
         return $this->headerCode;
     }
-    
+
+    /**
+     * Set header code
+     *
+     * @param $headerCode
+     * @return string
+     */
+    public function setHeaderCode($headerCode)
+    {
+        return $this->headerCode = $headerCode;
+    }
+
+    /**
+     * Get Type
+     *
+     * @return string
+     */
     public function getType()
     {
         return $this->type;
     }
-    
+
+    /**
+     * Set Type
+     *
+     * @param $type
+     * @return string
+     */
+    public function setType($type)
+    {
+        return $this->type = $type;
+    }
+
+    /**
+     * Get Serializer
+     *
+     * @return PacketSerializerInterface
+     */
     public function getSerializer()
     {
         return $this->serializer;
     }
-    
-    public function setSerializer(PacketSerializerInterface $serializer)
+
+    /**
+     * Set Serializer
+     *
+     * @param PacketSerializerInterface $serializer
+     * @return $this
+     */
+    public function setSerializer(PacketSerializerInterface $serializer = null)
     {
-        $this->serializer = $serializer;
+        $this->serializer = isset($serializer) ? $serializer : new static::$serializerClass;
         return $this;
     }
-    
+
+    /**
+     * Get Parser
+     *
+     * @return PacketParserInterface
+     */
     public function getParser()
     {
         return $this->parser;
     }
-    
-    public function setParser($parser)
+
+    /**
+     * Set Parser
+     *
+     * @param PacketParserInterface $parser
+     * @return $this
+     */
+    public function setParser(PacketParserInterface $parser)
     {
-        $this->parser = $parser;
+        $this->parser = isset($parser) ? $parser : new static::$parserClass;
         return $this;
     }
 }
